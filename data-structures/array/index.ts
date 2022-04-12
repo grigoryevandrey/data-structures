5e672dd5b1d1424944ddf6a1647ba1434b7d91eb
Andrey Grigoryev <adg97@yandex.ru>
2022-04-12 18:25:40 +0300

    feat: implemented array

--- data-structures/array/index.ts
+++ data-structures/array/index.ts
@@ -1,77 +1,60 @@
 // Lets imagine if arrays in JS were static
 
-// Implement resize on adding
-// Implement push method
-// Implement splice method
-// Implement pop method
-// Implement indexof method
-
 class MyArray<T> {
   private contents: T[];
-  private size: number;
+  private count: number = 0;
 
   constructor(size: number) {
-    if (size < 0 || size !== Math.floor(size)) {
+    if (size < 0 || size !== Math.floor(size))
       throw new RangeError('Invalid array length');
-    }
 
     this.contents = new Array(size);
-    this.size = size;
   }
 
   public insert(item: T): void {
-    for (let i = 0; i < this.size; i++) {
-      if (this.contents[i] === undefined) {
-        this.contents[i] = item;
-        return;
-      }
+    if (this.count === this.contents.length) {
+      const oldArr = [...this.contents];
+
+      this.contents = new Array(this.count + 1);
+
+      for (let i = 0; i < oldArr.length; i++) this.contents[i] = oldArr[i];
     }
 
-    const size = this.size + 1;
-    const newArray = new Array(size);
-
-    let i = 0;
-
-    for (i; i < size - 1; i++) {
-      newArray[i] = this.contents[i];
-    }
-
-    newArray[i] = item;
-
-    this.contents = newArray;
-    this.size = size;
+    this.contents[this.count] = item;
+    this.count++;
   }
 
   public removeAt(index: number): T {
-    const size = this.size - 1;
-    const newArray = new Array(size);
-    const removed = this.contents[index];
+    if (this.count <= index || index < 0 || Math.floor(index) !== index)
+      throw new RangeError('Invalid index');
 
-    for (let i = 0; i < size + 1; i++) {
+    const oldArr = [...this.contents];
+    this.contents = new Array(this.count - 1);
+
+    for (let i = 0; i < oldArr.length; i++) {
       if (i === index) continue;
 
       if (i < index) {
-        newArray[i] = this.contents[i];
+        this.contents[i] = oldArr[i];
         continue;
       }
 
-      newArray[i - 1] = this.contents[i];
+      this.contents[i - 1] = oldArr[i];
     }
 
-    this.contents = newArray;
-    this.size = size;
-
-    return removed;
+    this.count--;
+    return oldArr[index];
   }
 
   public indexOf(item: T): number {
-    for (let i = 0; i < this.size; i++) if (item === this.contents[i]) return i;
+    for (let i = 0; i < this.count; i++)
+      if (item === this.contents[i]) return i;
 
     return -1;
   }
 
   public print(): void {
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.count; i++) {
       console.log(this.contents[i]);
     }
   }
@@ -83,8 +66,14 @@ array.insert(10);
 array.insert(20);
 array.insert(30);
 
+array.print();
+
+console.log('');
+
 console.log(array.indexOf(30));
 console.log(array.removeAt(1));
 console.log(array.indexOf(30));
+
+console.log('');
 
 array.print();
